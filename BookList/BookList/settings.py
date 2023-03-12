ca68a4c2107eebde50fe1fe00156b278d3fccffd
hassan figuigui <hassan.figuigui7@aiesec.net>
2023-03-12 10:36:46 +0100

    creating ViewSet (Book)

--- BookList/BookList/settings.py
+++ BookList/BookList/settings.py
@@ -37,8 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'BookListAPI',   
     'rest_framework',
-    'BookListAPI'
+
 ]
 
 MIDDLEWARE = [
--- BookList/BookListAPI/views.py
+++ BookList/BookListAPI/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
+from rest_framework import viewsets
 
 # Create your views here.
-@api_view()
-def books(request):
-    return Response("List of the books",status = status.HTTP_200_OK)
+class Book(viewsets.ViewSet):
+	def books(self, request):
+		return Response({"message":"All books"},status.HTTP_200_OK)
+	
