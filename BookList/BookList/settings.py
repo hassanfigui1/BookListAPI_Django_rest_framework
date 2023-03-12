74220519f625ff5d142816bd95bd23d6327d8790
hassan figuigui <hassan.figuigui7@aiesec.net>
2023-03-12 06:43:21 +0100

    setting configurations for the rest framework and the BookListAPI app

--- BookList/BookList/settings.py
+++ BookList/BookList/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'BookListAPI',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
