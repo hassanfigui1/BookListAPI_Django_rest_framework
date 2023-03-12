6944caee860bea5cb9152f046cc74c278fdd5643
hassan figuigui <hassan.figuigui7@aiesec.net>
2023-03-12 10:56:30 +0100

    setting the mapping in the urls.py

--- BookList/BookListAPI/urls.py
+++ BookList/BookListAPI/urls.py
@@ -3,7 +3,14 @@ from . import views
 urlpatterns = [
     path('books/', views.Book.as_view(
         {
-            'get':'books'
+            'get':'books',
+            'post':'create'
         }
-    ))
+    )),
+    path('books/<int: pk>',views.Book.as_view({
+        'put':'update',
+        'path':'partial_update',
+        'delete':'destroy',
+        'get':'retrieve'
+    }))
 ]
