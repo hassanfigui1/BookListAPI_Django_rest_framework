627e90e263800ac0d3a11b63aef8f433cb34363e
hassan figuigui <hassan.figuigui7@aiesec.net>
2023-03-12 14:05:40 +0100

    mapping the ReadOnlyBook viewSets

--- BookList/BookListAPI/urls.py
+++ BookList/BookListAPI/urls.py
@@ -1,16 +1,19 @@
 from django.urls import path 
 from . import views 
 urlpatterns = [
-    path('books/', views.Book.as_view(
+    path('books/', views.ReadOnlyBooks.as_view(
         {
             'get':'list',
-            'post':'create'
+            # 'post':'create'
         }
     )),
+    path('books/<int:pk>',views.ReadOnlyBooks.as_view({
+        'get':'retreive',
+    })),
     path('books/<int:pk>',views.Book.as_view({
         'put':'update',
         'patch':'partial_update',
         'delete':'destroy',
-        'get':'retreive'
+
     }))
 ]
